b43401b4868372aa0ce536fc21888357e564d46e
saisoftsuave <sai.babu@softsuave.org>
2025-03-06 16:31:10 +0530

    network monitor optimised

--- composeApp/src/desktopMain/kotlin/NetworkMonitorDesktop.kt
+++ composeApp/src/desktopMain/kotlin/NetworkMonitorDesktop.kt
@@ -2,26 +2,30 @@ package org.softsuave.bustlespot
 
 import io.ktor.client.HttpClient
 import io.ktor.client.request.get
-import io.ktor.client.statement.HttpResponse
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import org.softsuave.bustlespot.network.NetworkMonitor
+import java.util.concurrent.atomic.AtomicBoolean
 
-
-class NetworkMonitorDesktop() : NetworkMonitor {
-    private val client = HttpClient()
+class NetworkMonitorDesktop : NetworkMonitor {
+    private val client: HttpClient by lazy { HttpClient() }
+    private val status = AtomicBoolean(false)
 
     override val isConnected: Flow<Boolean> = flow {
         while (true) {
-            try {
-                val response: HttpResponse = client.get("https://www.google.com")
-                emit(response.status.value == 200)
+            val newStatus = try {
+                client.get("https://www.google.com").status.value == 200
             } catch (e: Exception) {
-                emit(false)
+                false
+            }
+
+            if (status.getAndSet(newStatus) != newStatus) {
+                emit(newStatus)
+                println("Network status changed: $newStatus")
             }
             delay(5000)
         }
     }
-
 }
+
