ef66c057a0203bd209dea1cfb9328b67470860a3
SaiSrujan-SoftSuave <sai.srujan@softsuave.org>
2025-04-25 17:43:28 +0530

    reverted the timings

--- composeApp/src/desktopMain/kotlin/timer/TimerClass.desktop.kt
+++ composeApp/src/desktopMain/kotlin/timer/TimerClass.desktop.kt
@@ -33,7 +33,7 @@ actual class TrackerModule actual constructor(private val viewModelScope: Corout
     actual var keyboradKeyEvents: MutableStateFlow<Int> = MutableStateFlow(0)
     actual var mouseKeyEvents: MutableStateFlow<Int> = MutableStateFlow(0)
     actual var mouseMotionCount: MutableStateFlow<Int> = MutableStateFlow(0)
-    actual var customeTimeForIdleTime: MutableStateFlow<Int> = MutableStateFlow(15)
+    actual var customeTimeForIdleTime: MutableStateFlow<Int> = MutableStateFlow(480)
     actual var numberOfScreenshot: MutableStateFlow<Int> = MutableStateFlow(1)
     actual var isTrackerStarted: MutableStateFlow<Boolean> = MutableStateFlow(false)
 
@@ -58,8 +58,8 @@ actual class TrackerModule actual constructor(private val viewModelScope: Corout
     private val screenShotFrequency = 1 // n of screenshot in a slot
     private val screenshotLimit = 10 //in mints
     private var idealStartTime: Instant = Instant.DISTANT_PAST
-    private val postActivityInterval: Int = 30 //in second
-    private val storeActivityInterval: Int = 10 //in second
+    private val postActivityInterval: Int = 600 //in second
+    private val storeActivityInterval: Int = 60 //in second
 
     actual fun resetTimer() {
         isTrackerRunning.value = false
@@ -293,7 +293,7 @@ actual class TrackerModule actual constructor(private val viewModelScope: Corout
             totalActivity = (((mouseKeyEvents.value + keyboradKeyEvents.value) / intervalInSeconds.toFloat()) * 100).toInt(),
             billable = "",
             notes = "",
-//            uri = currentImageUri.value
+            uri = currentImageUri.value
         )
         startTime = endTime
         globalEventListener.resetClickCount()
