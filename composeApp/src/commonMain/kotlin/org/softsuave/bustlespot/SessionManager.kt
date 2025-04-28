bddbdd5bea5f938e147b8b66957e680c0403a74e
SaiSrujan-SoftSuave <sai.srujan@softsuave.org>
2025-04-28 10:44:17 +0530

    fixed version change and remove local db

--- composeApp/src/commonMain/kotlin/org/softsuave/bustlespot/SessionManager.kt
+++ composeApp/src/commonMain/kotlin/org/softsuave/bustlespot/SessionManager.kt
@@ -86,12 +86,12 @@ class SessionManager(
     }
 
     init {
-        SettingsHelper(settings).clearIfFirstRun()
+        SettingsHelper(settings,db).clearIfFirstRun()
         isLoggedIn = MutableStateFlow(settings.getString("access_token", "").isNotEmpty())
     }
 }
 
-class SettingsHelper(private val settings: ObservableSettings) {
+class SettingsHelper(private val settings: ObservableSettings,private val db:Database) {
     private val appVersionKey = "app_version"
 
     fun clearIfFirstRun() {
@@ -103,6 +103,9 @@ class SettingsHelper(private val settings: ObservableSettings) {
         if (isFirstRun) {
             // Clear all existing settings
             settings.clear()
+            db.organisationDatabaseQueries.deleteAllOrganisations()
+            db.activitiesDatabaseQueries.deleteAllActivities()
+            db.activitiesDatabaseQueries.deleteAllFailedActivities()
             // Mark first run as completed
             settings.putString(appVersionKey, currentVersion)
         }
