5d6cb3b9d04fb9e787e3e091d2aa8fd755a85392
SaiSrujan-SoftSuave <sai.srujan@softsuave.org>
2025-05-13 19:17:01 +0530

    Checked db failed calls issues

--- composeApp/src/commonMain/kotlin/org/softsuave/bustlespot/tracker/data/TrackerRepositoryImpl.kt
+++ composeApp/src/commonMain/kotlin/org/softsuave/bustlespot/tracker/data/TrackerRepositoryImpl.kt
@@ -114,7 +114,7 @@ class TrackerRepositoryImpl(
                         saveFailedPostUserActivity(postActivityRequest)
                         db.activitiesDatabaseQueries.deleteAllActivities()
                     }
-                    Log.d("---- activities are deleted on post failure ----")
+                    Log.d("---- activities are deleted on post failure ---- ${response.status} -- ${response.toString()} ")
                     emit(Result.Error(message = "Failed post activity: ${response.status}"))
                 }
             } catch (e: Exception) {
@@ -123,7 +123,8 @@ class TrackerRepositoryImpl(
                     saveFailedPostUserActivity(postActivityRequest)
                     db.activitiesDatabaseQueries.deleteAllActivities()
                 }
-                Log.d("---- activities are deleted on post failure ----")
+                Log.d("---- activities are deleted on post failure ---- ${e.message}")
+                e.printStackTrace()
                 emit(Result.Error(e.message ?: "Unknown error"))
             }
         }
